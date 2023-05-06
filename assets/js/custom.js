0a19d99c8ada7961b3d59397345b5913fbf12851
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-05-05 21:44:41 -0400

    #

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,28 +1,27 @@
-if($.isFunction('owlCarousel')){
-  $("#owl-demo").owlCarousel({
-    navigation : true
-  });
-}
-
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  dots:true,
-  autoplay:true,
-  autoplayTimeout:3000,
-  responsive:{
-      0:{
-          items:1
-      },
-      768:{
-          items:2
-      },
-      1280:{
-          items:3
+jQuery.noConflict();
+(function($) {
+  $(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      dots:true,
+      autoplay:true,
+      autoplayTimeout:3000,
+      responsive:{
+          0:{
+              items:1
+          },
+          768:{
+              items:2
+          },
+          1280:{
+              items:3
+          }
       }
-  }
-})
+    })
+  });
+})(jQuery);
 
 document.addEventListener("DOMContentLoaded", function(){
     window.addEventListener('scroll', function() {
