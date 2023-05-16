ed0feb2670842232ab7b507cd0b45fdcdf91c64e
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-05-16 18:41:49 -0400

    Logré mostrar index sin estilos

---
+++ enchanteur_store/static/assets/js/custom.js
@@ -0,0 +1,41 @@
+jQuery.noConflict();
+(function($) {
+  $(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      dots:true,
+      autoplay:true,
+      autoplayTimeout:3000,
+      responsive:{
+          0:{
+              items:1
+          },
+          768:{
+              items:2
+          },
+          1280:{
+              items:3
+          }
+      }
+    })
+  });
+})(jQuery);
+
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('navbar-top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar-top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
+
+
