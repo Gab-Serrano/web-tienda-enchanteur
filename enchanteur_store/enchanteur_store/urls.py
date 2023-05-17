e3bb16babe633530d8d060913b2711cd2d65137c
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-05-17 18:25:46 -0400

    Ajuste django. Agrega template store

--- enchanteur_store/enchanteur_store/urls.py
+++ enchanteur_store/enchanteur_store/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
+
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('store/', include('store.urls')),
 ]
