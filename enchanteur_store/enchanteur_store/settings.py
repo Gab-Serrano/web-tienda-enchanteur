b0e86980a3a918ab0d59f3b9f2850171cc326896
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-05-16 21:22:12 -0400

    #

--- enchanteur_store/enchanteur_store/settings.py
+++ enchanteur_store/enchanteur_store/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
