171f7c432dcc5909cf2aa46a35ce36944fe799e6
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-05-28 05:40:14 -0400

    renderizado de productos

--- enchanteur_store/enchanteur_store/settings.py
+++ enchanteur_store/enchanteur_store/settings.py
@@ -126,7 +126,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/assets/img')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
