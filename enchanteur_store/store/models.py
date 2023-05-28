1f376027aae91bf50372beb7592949c96a2e3f4b
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-05-28 06:00:37 -0400

    Actualiza modelo de prueba

--- enchanteur_store/store/models.py
+++ enchanteur_store/store/models.py
@@ -18,12 +18,19 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     data_orderd = models.DateTimeField(auto_now_add= True),
     complete = models.BooleanField(default=False, null=True, blank=False)
-    name = models.CharField(max_length = 200, null=True)
     transaction_id = models.CharField(max_length = 200, null=True)
     
     def __str__(self):
