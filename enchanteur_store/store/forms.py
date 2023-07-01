9cd9d90f429d4c897792081c7e63fdfe586cae46
DinoDG <dani.jaram@duocuc.cl>
2023-07-01 12:26:21 -0400

    por orden de Gabo

--- enchanteur_store/store/forms.py
+++ enchanteur_store/store/forms.py
@@ -3,7 +3,6 @@ from .models import Product
 from django.core.exceptions import ValidationError
 
 class ProductForm(forms.ModelForm):
-    descripcion = forms.CharField(widget=forms.Textarea)
 
     class Meta:
         
