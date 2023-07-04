adfdd457d6c9cb6588459c015023154d5c443f61
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-07-04 18:05:18 -0400

    Avance staff

--- enchanteur_store/store/views.py
+++ enchanteur_store/store/views.py
@@ -3,7 +3,6 @@ from .models import *
 from django.http import JsonResponse
 import json
 from . utils import cookieCart, guestOrder
-from .forms import ProductForm
 from .models import Product
 from django.shortcuts import redirect, get_object_or_404, reverse
 from django.contrib import messages
@@ -130,50 +129,3 @@ def cartCount (request):
         cookieData = cookieCart(request)
         cartItems = cookieData['cartItems']
     return cartItems
-
-def manageView(request):
-    products = Product.objects.all()
-    context = {'products': products,}
-    return render(request, 'store/manageView.html', context)
-
-def addProduct(request):
-    context = {}
-
-    form = ProductForm(request.POST or None, request.FILES or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Producto agregado correctamente')
-            context['success_message'] = True  # Agregar esta línea
-            return redirect('addProduct')
-        else:
-            print(form.errors)  # Imprime los errores en la consola
-            messages.warning(request, 'Error al agregar el producto')
-    
-    context['form'] = form
-
-    return render(request, 'store/addProduct.html', context)
-
-def editProduct(request, pk):
-    context = {}
-
-    obj = get_object_or_404(Product, pk=pk)
-    form = ProductForm(request.POST or None, request.FILES or None, instance=obj)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Producto editado correctamente')
-            context['success_message'] = True  # Agregar esta línea
-            return redirect('manageView')
-        else:
-            print(form.errors)  # Imprime los errores en la consola
-            messages.warning(request, 'Error al editar el producto')
-    
-    context['form'] = form
-
-    return render(request, 'store/editProduct.html', context)
-
-def deleteProduct(request, pk):
-    producto = get_object_or_404(Product, pk=pk)
-    producto.delete()
-    return redirect('manageView')
