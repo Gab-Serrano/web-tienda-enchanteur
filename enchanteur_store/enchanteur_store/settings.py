e025114501ce650a5cb07f5808c49a82403f4794
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-07-05 20:45:04 -0400

    migra dashboard básico

--- enchanteur_store/enchanteur_store/settings.py
+++ enchanteur_store/enchanteur_store/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'home.apps.HomeConfig',    
     'store.apps.StoreConfig',
+    'staff.apps.StaffConfig',
 ]
 
 MIDDLEWARE = [
--- enchanteur_store/enchanteur_store/urls.py
+++ enchanteur_store/enchanteur_store/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
 
     path('',include('home.urls')),    
     path('', include('store.urls')),
+    path('', include('staff.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
